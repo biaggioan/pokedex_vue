a2ebeabfae8be930a115a4a491ea9d900ce0cab6
biaggioan <50343885+biaggioan@users.noreply.github.com>
2021-09-29 11:27:45 -0300

    new deploy

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <PokemonList />
+  </div>
+</template>
+
+<script>
+import PokemonList from "../components/PokemonList";
+
+export default {
+  name: "PageIndex",
+  components: {
+    PokemonList,
+  },
+};
+</script>
